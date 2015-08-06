4106ae263cfedd7b99a6568eb23296b9d095603f
Carlos Bellotti <Carlos Bellotti>
2015-08-06 10:22:40 -0300

    Commit inicial

---
+++ public/js/verification-box.js
@@ -0,0 +1,19 @@
+function verificationBox(config) {
+
+	var div = $('<div class="v-box"/>');
+
+	var header = $('<div class="v-box-header" />');
+
+	div.append(header);
+
+
+	var cell = $('<div class="v-box-cell"/>');
+	cell.html(field);
+
+
+	var body = $('<div class="v-box-body" />');
+
+	div.append(body);
+
+
+}
